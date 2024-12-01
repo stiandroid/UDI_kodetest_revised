64d4a5c2cd723341c0f814f6fc42921b29ea1dff
stiandroid <stianen@hotmail.com>
2024-12-01 17:08:36 +0100

    Opprettet entitetsklasser og DTO-klasser

---
+++ UDI_kodetest_revised/Models/Entity/Person.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace UDI_kodetest_revised.Models.Entity;
+
+public class Person
+{
+    public Guid Id { get; set; }
+    public string Epost { get; set; } = string.Empty;
+    public string ExternalIdField { get; set; } = string.Empty;
+    public required string Fornavn { get; set; }
+    public required string Etternavn { get; set; }
+    public string Mellomnavn { get; set; } = string.Empty;
+    public required string Mobilnummer { get; set; }
+    public string Personnummer { get; set; } = string.Empty;
+    public required string Reisedokumentnummer { get; set; }
+    public required DateTime Fødselsdato { get; set; }
+    public string Adresse { get; set; } = string.Empty;
+    public string Poststed { get; set; } = string.Empty;
+    public string Postnummer { get; set; } = string.Empty;
+    public string Land { get; set; } = string.Empty;
+}
---
+++ UDI_kodetest_revised/Models/Entity/Sak.cs
@@ -0,0 +1,22 @@
+﻿namespace UDI_kodetest_revised.Models.Entity;
+
+public class Sak
+{
+    public Guid Id { get; set; }
+    public required string RefNo { get; set; }
+    public int Recno { get; set; }
+    public required string Sakid { get; set; }
+    public bool SendtSms { get; set; }
+
+    public Guid? KontaktId { get; set; }
+    public Person? Kontakt { get; set; }
+
+    public Guid? FullmektigId { get; set; }
+    public Person? Fullmektig { get; set; }
+
+    public Guid? SøkerId { get; set; }
+    public Person? Søker { get; set; }
+
+    public Guid? VedtaksId { get; set; }
+    public Vedtak? Vedtak { get; set; }
+}
