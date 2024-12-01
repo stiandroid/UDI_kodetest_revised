d1de4f27878c858eb6c2cc23b184eb5c96b36bf0
Stian Sæther <34171915+stiandroid@users.noreply.github.com>
2024-12-01 17:11:22 +0100

    Opprettet entitetsklasser og DTO-klasser (#1)
    Co-authored-by: stiandroid <stianen@hotmail.com>

---
+++ UDI_kodetest_revised/Models/DTO/PersonDetailsViewDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace UDI_kodetest_revised.Models.DTO;
+
+public class PersonDetailsViewDTO
+{
+    public string Epost { get; set; } = string.Empty;
+    public string Fornavn { get; set; } = string.Empty;
+    public string Etternavn { get; set; } = string.Empty;
+    public string Mellomnavn { get; set; } = string.Empty;
+    public string Mobilnummer { get; set; } = string.Empty;
+    public string Personnummer { get; set; } = string.Empty;
+    public string Reisedokumentnummer { get; set; } = string.Empty;
+    public DateTime Fødselsdato { get; set; }
+    public string Adresse { get; set; } = string.Empty;
+    public string Poststed { get; set; } = string.Empty;
+    public string Postnummer { get; set; } = string.Empty;
+    public string Land { get; set; } = string.Empty;
+}
